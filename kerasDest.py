a11219c6fae782a257c096b609965b5f53b929ed
Zygimantas <zygimantasmarma@gmail.com>
2021-10-20 15:52:04 +0300

    Testing

--- kerasDest.py
+++ kerasDest.py
@@ -1,80 +1,91 @@
 from tensorflow import keras
-from keras import optimizers
+#from keras import optimizers
 import numpy as np
 import matplotlib.pyplot as plt
 from keras.layers import Dense
 from keras.models import Sequential, load_model
 from numpy.core.numeric import True_
+#from keras.utils import to_categorical
+from tensorflow.keras.utils import to_categorical
+from tensorflow.keras import optimizers
  
 # from tensorflow import keras
 # from tensorflow.keras.layers import Dense
 # from tensorflow.keras.models import Sequential, load_model
  
-def getModel():
+def getModel(numberOffClasses = 3):
     model = Sequential()
+    model.add(Dense(units=16,activation='relu'))
     model.add(Dense(units=8,activation='relu'))
     model.add(Dense(units=4,activation='relu'))
-    model.add(Dense(units=1,activation='sigmoid'))
+    model.add(Dense(units=numberOffClasses, activation='softmax'))
     #'binary_crossentropy' - viena klase, 'sigmoid' 'tangent'
     # 'categorical_crossentropy' - daugiaklasis klasifikavimas, 'softmax'
     # 'mse' - reiksmes prognozavimui, 'sigmoid', 'pureline' 
-    model.compile(optimizer=keras.optimizers.Adam(),loss='binary_crossentropy',metrics=['accuracy']) 
+    #model.compile(optimizer=keras.optimizers.Adam(),loss='binary_crossentropy',metrics=['accuracy']) 
+    #model.compile(optimizer=keras.optimizers.rmsprop(),loss='categorical_crossentropy',metrics=['accuracy']) 
+    model.compile(optimizer=keras.optimizers.Adam(),loss='categorical_crossentropy',metrics=['accuracy']) 
+    #model.compile(optimizer=keras.optimizers.rmsprop(),loss=keras.losses.categorical_crossentropy,metrics=['accuracy']) 
     return model
  
-data1 = np.random.randn(100,2)
+number_of_samples = 100 
+data1 = np.random.randn(number_of_samples,2)
 rows1, cols1 = data1.shape
 y1 = np.zeros((rows1,1),dtype=np.float)
-data2 = np.random.randn(100,2)
+data2 = np.random.randn(number_of_samples,2)
 rows2, cols2 = data2.shape
 y2 = np.ones((rows2,1), dtype=np.float)
- 
+data3 = np.random.randn(number_of_samples,2)
+rows3, cols3 = data3.shape
+y3 = np.ones((rows3,1), dtype=np.float)*2
+
 data1[:,0] = data1[:,0] + 5
 data1[:,1] = data1[:,1] + 3
+
 data2[:,0] = data2[:,0] + 8 
 data2[:,1] = data2[:,1] + 1.5
+
+data3[:,0] = data3[:,0] + 10 
+data3[:,1] = data3[:,1] + 5
  
-trainData = np.vstack((data1,data2))
-trainY = np.vstack((y1,y2))
- 
+trainData = np.vstack((data1,data2,data3))
+trainY = np.vstack((y1,y2,y3))
+trainY = to_categorical(trainY,num_classes=3) 
 print(trainData.shape)
  
 train = False
 if train == True:
     model = getModel()
-    model.fit(x=trainData,y=trainY,epochs=100,batch_size=32)
+    model.fit(x=trainData, y=trainY, epochs=200, batch_size=32)
     model.summary()
-    model.save('modelis.h5')
+    model.save('modelis_multi.h5')
 else:
-    model = load_model('modelis.h5')
+    model = load_model('modelis_multi.h5')
  
     minX = np.min(trainData[:,0])
     maxX = np.max(trainData[:,0])
     minY = np.min(trainData[:,1])
     maxY = np.max(trainData[:,1])
  
-    stepsX = np.linspace(minX,maxX,100)
-    stepsY = np.linspace(minY,maxY,100)
+    stepsX = np.linspace(minX, maxX, 100)
+    stepsY = np.linspace(minY, maxY, 100)
 
     xx, yy = np.meshgrid(stepsX, stepsY)
 
     predictions = model.predict(np.c_[xx.ravel(), yy.ravel()])
+    predictions = np.where(predictions>0.5, predictions, 0)
+    predictions = np.where(predictions<0.5, predictions, 0)
+    z = predictions.reshape(xx.shape)
+
+    plt.contourf(xx, yy, z, cmap='Paired',alpha = 0.5)
+    
  
-#    for x in range(0,len(stepsX)):
-#        for y in range(0, len(stepsY)):
-#            xTest = np.array([[stepsX[x],stepsY[y]]], dtype=np.float)
-#            prediction = model.predict(xTest)
-#            if prediction[0][0] > 0.5:
-#                color = 'm'
-#            else:
-#                color = 'y'
-#            #plt.scatter(xTest[0,0],xTest[0,1],'gs',ms=20,markerfacecolor=color,markeredgecolor=color)
-#            plt.scatter(xTest[0,0],xTest[0,1],marker='s',color=color,s=10,alpha=0.5)
-#            print('x:{},y:{}'.format(x,y))
  
     print('prediction: {}'.format(predictions))
  
 plt.plot(data1[:,0],data1[:,1],'ro')
 plt.plot(data2[:,0],data2[:,1],'bo')
+plt.plot(data3[:,0],data3[:,1],'go')
 plt.grid(b=True)
 plt.xlabel('X')
 plt.ylabel('Y')
